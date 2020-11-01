01273d743c6d0093eb6828f05d963bdc7092dc65
pixdoet <63037136+pixdoet@users.noreply.github.com>
2020-11-01 22:45:56 +0800

    Update streaminggone.js

--- streaminggone.js
+++ streaminggone.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Remove Streaming Now
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Removes the top right corner's streaming now and featured people thingy
+// @version      0.2
+// @description  Removes the top right corner's streaming now and featured people thingy, as well as the news
 // @author       id_9982
 // @match        *://krunker.io/*
 // @exclude      *://krunker.io/social*
